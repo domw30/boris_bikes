0c20a141c39b03d3edc8235d1c487952331f91c9
Carly <carly.jenkinson65@googlemail.com>
2019-04-03 20:12:03 +0100

    chapter 11 - wroteand passed test for when docking station has no bikes

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,8 +1,21 @@
 require_relative "bike"
 
+
 class DockingStation
+  # attr_reader :bikes
+	def initialize
+		# @bikes = []
+	end
+
+	def bikes
+		[]
+	end
 
 	def release_bike
 		Bike.new
+	end
+
+	def dock_bike(bike)
+		# @bikes.push(bike)
 	end
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -2,7 +2,10 @@ require 'DockingStation'
 
 describe DockingStation do
 	it { is_expected.to respond_to :release_bike }
-
+  it { is_expected.to respond_to :dock_bike}
+	it "should include the :bikes attribute" do
+		expect(subject).to have_attributes(:bikes => [])
+	end
 	describe '#release_bike' do
 		it "should create a new Bike" do
 			bike = subject.release_bike
@@ -10,3 +13,7 @@ describe DockingStation do
 		end
 	end
 end
+
+# pass Bike instance into DockingStation instance
+# store Bike instances as an attribute of the DockingStation instance using instance variable
+# use attribute_reader to read an attribute n an instantiated object
