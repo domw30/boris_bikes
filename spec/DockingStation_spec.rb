aa9db98e63a7b094acf5abd6e82236ccf850fe9a
Jose Quinones <josemanuelqn.88@gmail.com>
2019-04-03 16:36:44 +0100

    Docking station now returns a bike

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -2,4 +2,11 @@ require 'DockingStation'
 
 describe DockingStation do
 	it { is_expected.to respond_to :release_bike }
+
+	describe '#release_bike' do
+		it "should create a new Bike" do
+			bike = subject.release_bike
+			expect(bike).to be_working
+		end
+	end
 end
