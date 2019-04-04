f847e79c04736fc71cca3431f85992bb5ca43c59
domw30 <domw30@gmail.com>
2019-04-04 15:53:09 +0100

    create new dock bike method, add attr_reader :bikes

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -3,6 +3,8 @@ require 'DockingStation'
 describe DockingStation do
 	it { is_expected.to respond_to :release_bike }
 	it { is_expected.to respond_to :dock_bike }
+	it { is_expected.to respond_to(:dock_bike).with(1).argument }
+	it { is_expected.to respond_to :bikes}
 
 	describe '#release_bike' do
 		it "should create a new Bike" do
@@ -10,12 +12,21 @@ describe DockingStation do
 			expect(bike).to be_working
 		end
 	end
+
+	describe '#dock_bike' do
+		it 'should store a bike on the bikes attr' do
+			bike = Bike.new
+			docking_station = DockingStation.new
+			docking_station.dock_bike(bike)
+			expect(docking_station.bikes).to eq bike
+		end
+	end
+
 end
 
 # respond to dock_bike
 # have_attributes bikes = []
-# 
+#
 
 # station = DockingStation.new
 # bike = station.release_bike
-
