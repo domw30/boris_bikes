b22f6c0f7f65d3ac7a8e4d69825fc3ae76fca43d
Jose Quinones <josemanuelqn.88@gmail.com>
2019-04-04 14:19:12 +0100

    tidy up

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -2,24 +2,9 @@ require_relative "bike"
 
 
 class DockingStation
-  # attr_reader :bikes
-	def initialize
-		# @bikes = []
-	end
-
-	def bikes
-		[]
-	end
 
 	def release_bike
 		Bike.new
 	end
 
-<<<<<<< HEAD
-	def dock_bike
-=======
-	def dock_bike(bike)
-		# @bikes.push(bike)
->>>>>>> 0c20a141c39b03d3edc8235d1c487952331f91c9
-	end
 end
